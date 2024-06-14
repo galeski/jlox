47c21fc88d477f7bc2ed086b7d012e97416022c3
galeski <mgaleski@gmail.com>
2024-06-14 16:10:18 +0200

    WIP
    overwhelming

--- lox.js
+++ lox.js
@@ -4,6 +4,10 @@ const readline = require('readline');
 
 // TODO:
 // [done ?] FIX LEXEME reading
+// [] Add support to Lox’s scanner for C-style /* ... */ block comments
+// [] Extract scanner to a new source file
+// [] Create AST Printer class from:
+// https://craftinginterpreters.com/representing-code.html
 
 // For now I will probably use an object instead of a set, because it
 // more closely follows javas enums
@@ -88,6 +92,215 @@ class Token {
     return `${this.type} ${this.lexeme} ${this.literal}`;
   }
 }
+
+class Expr {
+  constructor(left, operator, right) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+}
+
+class ExprBinary {
+  constructor(left, operator, right) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+}
+
+class ExprUnary {
+  constructor(operator, right) {
+    this.operator = operator;
+    this.right = right;
+  }
+}
+
+class ExprLiteral {
+  constructor(literal) {
+    this.literal = literal;
+  }
+}
+
+class ExprGrouping {
+  constructor(grouping) {
+    this.grouping = grouping;
+  }
+}
+
+class Parser {
+  // #tokens;
+  #current = 0;
+
+  constructor(tokens) {
+    this.tokens = tokens;
+  }
+
+  expression() {
+    return this.equality();
+  }
+
+  parse() {
+    try {
+      return this.expression();
+    } catch (error) {
+      if (error.name === "ParseError") return null;
+    }
+  }
+
+  equality() {
+    let expr = this.comparison();
+
+    while (this.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      let operator = this.previous();
+      let right = this.comparison();
+      expr = new ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  comparison() {
+    let expr = this.term();
+
+    while (this.match(TokenType.GREATER, TokenType.GREATER_EQUAL,
+      TokenType.LESS, TokenType.LESS_EQUAL
+    )) {
+      let operator = this.previous();
+      let right = this.term();
+      expr = new ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  // addition and subtraction
+  term() {
+    let expr = this.factor();
+
+    while (this.match(TokenType.MINUS, TokenType.PLUS)) {
+      let operator = this.previous();
+      let right = this.factor();
+      expr = new ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  // multiplication and division
+  factor() {
+    let expr = this.unary();
+
+    while (this.match(TokenType.SLASH, TokenType.STAR)) {
+      let operator = this.previous();
+      let right = this.unary();
+      expr = new ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  unary() {
+    if (this.match(TokenType.BANG, TokenType.MINUS)) {
+      let operator = this.previous();
+      let right = this.unary();
+      return new ExprUnary(operator, right);
+    }
+
+    return this.primary();
+  }
+
+  primary() {
+    if (this.match(TokenType.FALSE)) return new ExprLiteral(false);
+    if (this.match(TokenType.TRUE)) return new ExprLiteral(true);
+    if (this.match(TokenType.NIL)) return new ExprLiteral(null);
+
+    if (this.match(TokenType.NUMBER, TokenType.STRING)) {
+      return new ExprLiteral(this.previous().literal);
+    }
+
+    if (this.match(TokenType.LEFT_PAREN)) {
+      let expr = this.expression();
+      consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+      return new ExprGrouping(expr);
+    }
+
+    throw this.error(this.peek(), "Expect expression.");
+  }
+
+  match(...types) {
+    for (let type of types) {
+      if (this.check(type)) {
+        this.advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  consume(type, message) {
+    if (this.check(type)) return this.advance();
+
+    throw error(this.peek(), message);
+  }
+
+  check(type) {
+    if (this.isAtEnd()) return false;
+    return this.peek().type = type; // eeee...
+  }
+
+  advance() {
+    if (!this.isAtEnd()) this.#current++;
+    return this.previous();
+  }
+
+  isAtEnd() {
+    return this.peek().type == TokenType.EOF;
+  }
+
+  peek() {
+    return this.tokens[current];
+  }
+
+  previous() {
+    return tokens[current - 1];
+  }
+
+  error(token, message) {
+    Lox.parseError(token, message);
+    return new ParseError();
+  }
+
+  synchronize() {
+    this.advance();
+
+    while (!this.isAtEnd()) {
+      if (this.previous().type == TokenType.SEMICOLON) return;
+
+      switch (this.peek().type) {
+        case TokenType.CLASS:
+        case TokenType.FUN:
+        case TokenType.VAR:
+        case TokenType.FOR:
+        case TokenType.IF:
+        case TokenType.WHILE:
+        case TokenType.PRINT:
+        case TokenType.RETURN:
+        case TokenType.DEFAULT:
+          return;
+      }
+    }
+
+    this.advance();
+  }
+}
+
+// TODO: should this be somehow moved into Parser class???
+class ParseError extends Error {
+  constructor() {
+    this.name = "ParseError";
+  }
+};
 
 class Scanner {
   // source;
@@ -325,6 +538,11 @@ class Lox {
     const scanner = new Scanner(source);
     const tokens = scanner.scanTokens();
 
+    const parser = new Parser(tokens);
+    const expression = parser.parse();
+
+    if (this.hadError) return;
+
     for (let token of tokens) {
       console.log(token);
     }
@@ -332,6 +550,14 @@ class Lox {
 
   static error(line, message) {
     this.#report(line, "", message);
+  }
+
+  static parseError(token, message) {
+    if (token.type === TokenType.EOF) {
+      this.#report(token.line, " at end", message);
+    } else {
+      this.#report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 
   static #report(line, where, message) {
